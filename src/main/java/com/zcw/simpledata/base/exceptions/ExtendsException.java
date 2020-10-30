67b5718b78acbd1adbf473ca82621c9fc5da7be8
章宸玮 <530335286@qq.com>
2020-10-30 23:10:29 +0800

    增加了异常处理AOP

--- src/main/java/com/zcw/simpledata/base/exceptions/ExtendsException.java
+++
@@ -1,9 +0,0 @@
-package com.zcw.simpledata.base.exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public class ExtendsException extends ApiException{
-    public ExtendsException(String message) {
-        super(HttpStatus.BAD_REQUEST, message);
-    }
-}
