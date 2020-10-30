bc9b280b9c4672f6e6cacc35438cfc070cba9a0f
Jiuchen <1971719180@qq.com>
2020-10-30 23:52:39 +0800

    启动类包名修改 .idea .iml配置修改

--- src/main/java/com/simpledata/frame/SimpleDataApplication.java
+++ src/main/java/com/simpledata/frame/SimpleDataApplication.java
@@ -1,4 +1,4 @@
-package com.zcw.simpledata;
+package com.simpledata.frame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
