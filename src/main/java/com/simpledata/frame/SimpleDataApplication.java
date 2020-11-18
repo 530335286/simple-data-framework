f77694d6f8194ea237451a126eac1a6298f85576
Jiuchen <1971719180@qq.com>
2020-11-18 10:40:03 +0800

    判断是否表中没有主键 如果没有则终止生成类 By:Jiuchen

--- src/main/java/com/simpledata/frame/SimpleDataApplication.java
+++ src/main/java/com/simpledata/frame/SimpleDataApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /***
- * simple-data
+ * simple-data1.0
  * @author zcw
  * @version 1.0
  */
